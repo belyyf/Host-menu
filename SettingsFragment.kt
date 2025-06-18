6d814c1a3bd0d961cf46eb97b9bda951fa86b62f
belyyfh <belyjegor45@gmail.com>
2025-06-18 10:28:14 +0300

    Create SettingsFragment.kt

---
+++ SettingsFragment.kt
@@ -0,0 +1,9 @@
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.example.mysettingsapp.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
